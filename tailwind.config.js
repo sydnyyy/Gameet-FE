e3816a84f654683220404ff61c89a1d9109e9486
coo1river <longpaca@gmail.com>
2025-04-17 03:32:22 +0900

    Style: tailwind.config.js 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,27 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      spacing: Object.fromEntries(
-        Array.from({ length: 5001 }, (_, i) => [i.toString(), `${i}px`]),
-      ),
+      spacing: Object.fromEntries(Array.from({ length: 5001 }, (_, i) => [i.toString(), `${i}px`])),
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        sans: ['Inter', 'sans-serif'],
       },
     },
   },
   plugins: [
     plugin(function ({ addComponents }) {
       addComponents({
-        ".flex-center": {
-          display: "flex",
-          "align-items": "center",
-          "justify-content": "center",
-        },
-        ".flex-between": {
-          display: "flex",
-          "align-items": "center",
-          "justify-content": "space-between",
+        '.flex-center': {
+          display: 'flex',
+          'align-items': 'center',
+          'justify-content': 'center',
         },
       });
     }),
