7bc8cbc74f261dd74f66d9aaf7f231518291c219
jjamva <hongi97@naver.com>
2025-04-19 00:39:00 +0900

    Style: prttier 셋팅 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   semi: true, // 세미콜론 붙임
-  singleQuote: true, // 문자열은 ' 사용
+  doubleQuote: true, // 문자열은 ' 사용
   tabWidth: 2, // 들여쓰기 2칸
   printWidth: 100, // 줄 길이 제한
   trailingComma: "all", // 마지막 쉼표도 붙이기
   arrowParens: "avoid", // 괄호 생략
   bracketSpacing: true, // 객체 띄어쓰기
-  endOfLine: "lf", // 유닉스 줄바꿈 
-};  
+  endOfLine: "lf", // 유닉스 줄바꿈
+};
