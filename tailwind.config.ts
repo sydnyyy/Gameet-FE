408d8faff7f30b551c202487c1b09d9515bb4492
coo1river <longpaca@gmail.com>
2025-05-02 22:35:35 +0900

    Chore: HeroUI 설치, 설정 및 Tailwind 다운그레이드

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
+const { heroui } = require("@heroui/react");
 
 const config: Config = {
   content: [
@@ -7,6 +8,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,7 +18,7 @@ const config: Config = {
     },
   },
   plugins: [
-    plugin(({ addComponents }) => {
+    plugin(function ({ addComponents }) {
       addComponents({
         ".flex-center": {
           display: "flex",
@@ -25,6 +27,7 @@ const config: Config = {
         },
       });
     }),
+    heroui(),
   ],
 };
 
