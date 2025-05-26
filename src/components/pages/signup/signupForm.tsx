c1cd2f470091f232be74ce4d62f1a3c38b1b6e27
coo1river <longpaca@gmail.com>
2025-05-27 05:32:29 +0900

    Fix: 회원가입 버튼 활성화 예외 처리

--- src/components/pages/signup/signupForm.tsx
+++ src/components/pages/signup/signupForm.tsx
@@ -7,29 +7,37 @@ import { useSignUpForm } from "@/hooks/pages/signUp/useSignUpForm";
 import FormLayout from "@/components/form/formLayout";
 
 export default function SignUpForm() {
-  const { methods, onSubmit, error, handleSendEmailAuth, handleVerifyEmailAuth } = useSignUpForm(); // 회원가입, 이메일 인증 함수
+  const {
+    methods,
+    onSubmit,
+    isEmailSend,
+    isEmailVerify,
+    signUpError,
+    sendEmailAuth,
+    verifyEmailAuth,
+  } = useSignUpForm(); // 회원가입 폼 함수 가져오기
 
   const password = methods.watch("password");
+  const { errors, isValid } = methods.formState;
 
   return (
-    <FormLayout methods={methods} onSubmit={onSubmit} error={error}>
+    <FormLayout methods={methods} onSubmit={onSubmit} error={signUpError}>
       <div className="flex gap-2">
         <Inputs name="email" type="email" label="이메일" rules={emailValid} />
-        <Buttons type="button" className="h-[48px] my-1" onClick={handleSendEmailAuth}>
-          인증하기
+        <Buttons type="button" className="h-[48px] my-1" onClick={sendEmailAuth}>
+          {isEmailSend ? "재요청" : "인증하기"}
         </Buttons>
       </div>
 
       <div className="flex gap-2">
-        <Inputs
-          name="authCode"
-          label="인증번호"
-          type="numeric"
-          inputMode="numeric"
-          rules={authCodeValid}
-        />
-        <Buttons type="button" className="h-[48px] my-1" onClick={handleVerifyEmailAuth}>
-          인증 확인
+        <Inputs name="authCode" label="인증번호" type="string" rules={authCodeValid} />
+        <Buttons
+          type="button"
+          className="h-[48px] my-1"
+          onClick={verifyEmailAuth}
+          isDisabled={!!isEmailVerify}
+        >
+          {isEmailVerify ? "인증 완료" : "인증 확인"}
         </Buttons>
       </div>
 
@@ -43,10 +51,15 @@ export default function SignUpForm() {
 
       {/* 약관 동의 */}
       <TermsArgreement />
-      <Buttons type="submit" size="lg" isDisabled={!methods.formState.isValid}>
+
+      <Buttons
+        type="submit"
+        size="lg"
+        isDisabled={!isValid || Object.keys(errors).length > 0 || !isEmailSend || !isEmailVerify}
+      >
         회원가입
       </Buttons>
-      {error && <p className="text-red-500">{error}</p>}
+      {signUpError && <p className="text-red-500">{signUpError}</p>}
     </FormLayout>
   );
 }
