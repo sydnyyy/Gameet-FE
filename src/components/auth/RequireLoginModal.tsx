c85f642176eb58555b29f15072b2b7fa322b1b55
coo1river <longpaca@gmail.com>
2025-06-12 04:20:36 +0900

    Feat: 로그인 가드 모달 컴포넌트 구현

---
+++ src/components/auth/RequireLoginModal.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useEffect } from "react";
+import { useAuth } from "@/hooks/common/useAuth";
+import { useModal } from "@/hooks/modal/useModal";
+import { useRouter } from "next/navigation";
+import Buttons from "../common/button/Buttons";
+
+export default function RequireLoginModal() {
+  const { isLogin } = useAuth();
+  const { onOpen, onClose, Modal } = useModal();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLogin) {
+      onOpen();
+    }
+  }, [isLogin, onOpen]);
+
+  if (isLogin) return null;
+
+  const handleGoLogin = () => {
+    onClose();
+    router.push("/login?redirect=" + encodeURIComponent(window.location.pathname));
+  };
+
+  return (
+    <Modal headerText="💡 알림">
+      <>
+        로그인이 필요합니다.
+        <Buttons onClick={handleGoLogin}>로그인</Buttons>
+      </>
+    </Modal>
+  );
+}
