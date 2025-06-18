a32c50fad6f4ff227c9b7192035bcb7d075184e0
hyemi.kim <hemi33379@gmail.com>
2025-06-18 10:54:27 +0900

    Feat: 매칭 종료 시 상태 변경 및 구독자에게 STOMP 알림 전송

--- src/hooks/pages/match/useMatchNotification.ts
+++ src/hooks/pages/match/useMatchNotification.ts
@@ -10,7 +10,7 @@ export const useMatchNotificationHandler = () => {
   // 매칭 알림 메시지 관리
   const handleMatchNotification = (notificationData: MatchStatusType) => {
     const status = notificationData.match_status;
-    if (status === "MATCHED" || status === "FAILED") {
+    if (status === "MATCHED" || status === "FAILED" || status === "COMPLETED") {
       const newMatchStatus: MatchStatusType = {
         match_status: notificationData.match_status,
         match_room_id: notificationData.match_room_id,
@@ -19,6 +19,10 @@ export const useMatchNotificationHandler = () => {
 
       queryClient.setQueryData(matchQueryKeys.status(), newMatchStatus);
       console.log("캐시 업데이트:", newMatchStatus);
+
+      if (status === "COMPLETED") {
+        router.push("/");
+      }
 
       // if (notificationData.match_status === "MATCHED" && notificationData.match_room_id) {
       //   router.push(`/chat/${notificationData.match_room_id}`);
