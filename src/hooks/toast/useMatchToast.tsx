e2441e168a65cb9865f20b2575c1deaf2c5c9b26
coo1river <longpaca@gmail.com>
2025-06-18 16:13:25 +0900

    Refactor: 채팅 구현에 따른 코드 수정, 매칭 페이지 내 toast 알림 비활성화

--- src/hooks/toast/useMatchToast.tsx
+++ src/hooks/toast/useMatchToast.tsx
@@ -3,28 +3,29 @@ import Buttons from "@/components/common/button/Buttons";
 import { useEffect } from "react";
 import { useMatchStatusStore } from "@/store/useMatchStatus";
 import useToast from "../toast/useToast";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function useMatchToast() {
   const { status, clearStatus } = useMatchStatusStore();
   const showToast = useToast();
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
+    if (pathname === "/match") {
+      return;
+    }
+
     if (status?.match_status === "MATCHED" && status.match_room_id) {
       showToast({
         title: "매칭 완료!",
         description: "매칭이 완료되었습니다.",
         buttonContent: (
-          <Buttons
-            children="채팅방으로 이동"
-            size="sm"
-            onClick={() => router.push(`chat/${status.match_room_id}`)}
-          />
+          <Buttons children="채팅방으로 이동" size="sm" onClick={() => router.push("/match")} />
         ),
       });
 
       clearStatus();
     }
-  }, [status, clearStatus]);
+  }, [status, pathname, clearStatus]);
 }
