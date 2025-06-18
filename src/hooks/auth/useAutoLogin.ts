0ffa8f9f6bf2bc28107d217e2789d268d73e213b
coo1river <longpaca@gmail.com>
2025-06-18 16:21:17 +0900

    Refactor: useAuthLogin 훅 ts로 변경

---
+++ src/hooks/auth/useAutoLogin.ts
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect } from "react";
+import { useAuthStore } from "@/store/useAuthStore";
+import { apiRequest } from "@/app/api/apiRequest";
+
+export default function useAutoLogin() {
+  const { setToken, clearToken, _hasHydrated, rememberMe, token } = useAuthStore();
+
+  useEffect(() => {
+    if (!_hasHydrated || !rememberMe || !token) {
+      return;
+    }
+
+    async function checkRefreshToken() {
+      const { rememberMe } = useAuthStore.getState();
+      if (!rememberMe) return;
+      try {
+        const res = await apiRequest("/users/auth/token/refresh", "POST", {}, { skipAuth: true });
+        const newToken = res.headers.authorization;
+        if (newToken) {
+          setToken(newToken);
+        }
+      } catch (error) {
+        clearToken();
+      }
+    }
+    checkRefreshToken();
+  }, [setToken, clearToken]);
+}
